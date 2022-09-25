bb677926e99f50dc62fa6c0514eb53703a6c63f6
cockrochi <mrardaacar@hotmail.com>
2022-09-25 18:32:54 +0300

    Add files via upload

---
+++ versions_src/0.0.1-INIT sources/cockrochihcore/0.0.2 Sources/src/main/java/me/cockrochi/cockrochihcore/EventListeners/LookAtLog.java
@@ -0,0 +1,35 @@
+package me.cockrochi.cockrochihcore.EventListeners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import java.util.List;
+
+public class LookAtLog implements Listener {
+
+    @EventHandler
+    public void onLookAtLog(PlayerMoveEvent e ){
+        Player p = e.getPlayer();
+        double h = p.getHealth();
+        List<Block> pLineOfSight = p.getLineOfSight(null, 70);
+
+        for (Block b : pLineOfSight){
+            Material m = b.getType();
+
+            if (m == Material.OAK_LOG){
+                if (h > 1){
+                    p.setHealth(h - 1.0);
+                } else{
+                    p.setHealth(0.0);
+                }
+                p.sendMessage(ChatColor.RED + "STOP LOOKING AT OAK TREES");
+            }
+        }
+
+    }
+}
